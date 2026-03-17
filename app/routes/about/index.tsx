3a9b263848d9dc3b2bfbdca50c61bf3e0609c418
Chantell Nichols <chantellnichols13@gmail.com>
2026-03-17 19:59:17 -0400

    Layout Routes

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,11 +1,11 @@
 const AboutPage = () => {
-    return (
-    <section>
-        <h2 className="text-3xl font-bold text-white mb-2">
-            Hey, I'm Chantell 👋🏽
-        </h2>
-    </section>
-    );
-}
- 
+  return (
+    <>
+      <h2 className="text-3xl font-bold text-white mb-2">
+        Hey, I'm Chantell 👋🏽
+      </h2>
+    </>
+  );
+};
+
 export default AboutPage;
