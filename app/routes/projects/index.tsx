11214632a097d0409926e68619f4e61108c677f9
Chantell Nichols <chantellnichols13@gmail.com>
2026-03-20 22:32:25 -0400

    Display Loader Data

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,19 +1,27 @@
 import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import ProjectCard from "~/components/ProjectCard";
 
-export async function loader({ request }:Route.LoaderArgs):Promise<any> {
+export async function loader({ request, }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch('http://localhost:8000/projects');
   const data = await res.json();
 
-  return {projects: data};
+  return { projects: data };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData;
-  console.log(projects);
+  const { projects } = loaderData as {projects: Project[]};
+  
 
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project}/>
+        ))}
+      </div>
     </>
   );
 };
