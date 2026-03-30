9942544c5da4676a310905fd8fa32a7650baeccc
Chantell Nichols <chantellnichols13@gmail.com>
2026-03-29 23:38:08 -0400

    Re-Use Pagination Component

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -16,7 +16,7 @@ const Pagination: React.FC<PaginationProps> = ({
         <button 
         key={idx + 1}
         className={`px-3 py-1 cursor-pointer rounded ${
-          currentPage === idx + 1 ? 'bg-blue-60 text-white' : 'bg-gray-700 text-gray-200'
+          currentPage === idx + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'
         }`}
         onClick={  () => onPageChange(idx + 1) }
         >
