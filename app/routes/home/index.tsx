c0cd89c3c6b51ecab36c0708815bc0d1cd928a12
Chantell Nichols <chantellnichols13@gmail.com>
2026-04-10 22:08:32 -0400

    Fetch Projects From Strapi

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -26,12 +26,24 @@ export async function loader({
     throw new Error('Failed to fetch projects or posts')
   }
 
-  const [projects, posts] = await Promise.all([
+  const [projectJson, posts] = await Promise.all([
     projectRes.json(),
     postRes.json()
   ])
 
-  return { projects, posts };
+  const projects = projectJson.data.map((item) => ({
+  id: item.id,
+  documentId: item.documentId,
+  title: item.title,
+  description: item.description,
+  image: item.image?.url ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}` : '/images/no-image.png',
+  url: item.url,
+  date: item.date,
+  category: item.category,
+  featured: item.featured
+}))
+
+return { projects, posts }
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
